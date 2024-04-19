f1bf0533fdde432869b79fd610bdd51f0498b443
Renan Gomes <renango2015@gmail.com>
2024-04-19 16:29:45 -0300

    style: fix 'SideMenu' button overflow

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -33,7 +33,7 @@ export const SideMenu = ({ session }: { session: Session }) => {
         </SheetHeader>
         <div className='mt-auto grid py-6'>
           {!isAtHome && (
-            <Button variant='outline' className='mb-4 py-6'>
+            <Button variant='outline' className='relative mb-4 py-6'>
               <Link className='absolute flex h-full w-full items-center justify-center' href='/'>
                 Início
               </Link>
