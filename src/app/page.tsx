1d6944be9b437cd1fe4f08b5cc183e330bba80b8
Renan Gomes <renango2015@gmail.com>
2024-04-20 19:23:21 -0300

    style: improve mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,11 @@ export default async function Home() {
     <Fragment>
       <Navbar />
       <div className='flex flex-col items-start px-28 pb-20 pt-14 max-xl:px-20 max-md:px-6 max-md:pb-12 max-md:pt-10'>
-        <div className='mb-6 flex w-full justify-between gap-6 max-md:mb-4 max-sm:flex-col'>
+        <div className='mb-6 flex w-full justify-between gap-6 px-2 max-md:mb-4 max-sm:flex-col'>
           <div className='flex flex-col'>
-            <h1 className='w-full font-galada text-6xl font-medium max-lg:text-4xl'>Barbearia Sr. Xavier</h1>
+            <h1 className='w-full font-galada text-6xl font-medium max-lg:text-[44px]'>
+              Barbearia Sr. Xavier
+            </h1>
             <p className='text-base  font-light max-md:text-sm'>
               Cortes Tradicionais e Atendimento Personalizado
             </p>
